14ca6acbf4cd94d415fb20327d13bfcc676ea389
minhtrn98 <minh.tran.cong@247express.vn>
2026-07-13 08:06:26 +0700

    refactor: streamline database connection handling in ApplicationDbQuery fix: update MudJsonField to use _innerValue for value binding

--- TMS.DeveloperTool.Blazor/Infrastructure/Data/ApplicationDbQuery.cs
+++ TMS.DeveloperTool.Blazor/Infrastructure/Data/ApplicationDbQuery.cs
@@ -8,87 +8,69 @@ namespace TMS.DeveloperTool.Blazor.Infrastructure.Data;
 public sealed partial class ApplicationDbQuery(IConfiguration configuration, ILogger<ApplicationDbQuery> logger, string database)
 {
     private readonly string? _connectionString = configuration.GetConnectionString(database);
-    private NpgsqlConnection? _connection;
 
     public async Task<T?> SingleOrDefaultAsync<T>(string sql, object? parameters, CancellationToken cancellationToken)
     {
-        _connection ??= new NpgsqlConnection(_connectionString);
-        if (_connection.State != ConnectionState.Open)
-        {
-            await _connection.OpenAsync(cancellationToken);
-        }
+        await using NpgsqlConnection connection = new(_connectionString);
+        await connection.OpenAsync(cancellationToken);
         CommandDefinition commandDefinition = new(
             commandText: sql,
             parameters: parameters,
             cancellationToken: cancellationToken
         );
         LogSqlQuery(sql);
-        return await _connection.QuerySingleOrDefaultAsync<T>(commandDefinition);
+        return await connection.QuerySingleOrDefaultAsync<T>(commandDefinition);
     }
 
     public async Task<T?> FirstOrDefaultAsync<T>(string sql, object? parameters, CancellationToken cancellationToken)
     {
-        _connection ??= new NpgsqlConnection(_connectionString);
-        if (_connection.State != ConnectionState.Open)
-        {
-            await _connection.OpenAsync(cancellationToken);
-        }
+        await using NpgsqlConnection connection = new(_connectionString);
+        await connection.OpenAsync(cancellationToken);
         CommandDefinition commandDefinition = new(
             commandText: sql,
             parameters: parameters,
             cancellationToken: cancellationToken
         );
         LogSqlQuery(sql);
-        return await _connection.QueryFirstOrDefaultAsync<T>(commandDefinition);
+        return await connection.QueryFirstOrDefaultAsync<T>(commandDefinition);
     }
 
     public async Task<IEnumerable<T>> QueryAsync<T>(string sql, object? parameters, CancellationToken cancellationToken)
     {
-        _connection ??= new NpgsqlConnection(_connectionString);
-        if (_connection.State != ConnectionState.Open)
-        {
-            await _connection.OpenAsync(cancellationToken);
-        }
-
+        await using NpgsqlConnection connection = new(_connectionString);
+        await connection.OpenAsync(cancellationToken);
         CommandDefinition commandDefinition = new(
             commandText: sql,
             parameters: parameters,
             cancellationToken: cancellationToken
         );
         LogSqlQuery(sql);
-        return await _connection.QueryAsync<T>(commandDefinition);
+        return await connection.QueryAsync<T>(commandDefinition);
     }
 
     public async Task<IEnumerable<dynamic>> QueryAsync(string sql, object? parameters, CancellationToken cancellationToken)
     {
-        _connection ??= new NpgsqlConnection(_connectionString);
-        if (_connection.State != ConnectionState.Open)
-        {
-            await _connection.OpenAsync(cancellationToken);
-        }
-
+        await using NpgsqlConnection connection = new(_connectionString);
+        await connection.OpenAsync(cancellationToken);
         CommandDefinition commandDefinition = new(
             commandText: sql,
             parameters: parameters,
             cancellationToken: cancellationToken
         );
         LogSqlQuery(sql);
-        return await _connection.QueryAsync(commandDefinition);
+        return await connection.QueryAsync(commandDefinition);
     }
 
-    public IAsyncEnumerable<T> QueryUnbufferedAsync<T>(string sql, object? parameters = null)
+    public async IAsyncEnumerable<T> QueryUnbufferedAsync<T>(string sql, object? parameters = null)
     {
-        _connection ??= new NpgsqlConnection(_connectionString);
-        return _connection.QueryUnbufferedAsync<T>(sql, parameters);
-    }
-
-    public async ValueTask DisposeAsync()
-    {
-        if (_connection != null)
+        await using NpgsqlConnection connection = new(_connectionString);
+        await foreach (T item in connection.QueryUnbufferedAsync<T>(sql, parameters))
         {
-            await _connection.DisposeAsync();
+            yield return item;
         }
     }
+
+    public ValueTask DisposeAsync() => ValueTask.CompletedTask;
 
     [LoggerMessage(Level = LogLevel.Information, Message = "[DAPPER] Executing SQL query:\n{Sql}")]
     private partial void LogSqlQuery(string sql);
